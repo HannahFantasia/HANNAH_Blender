60455bcceb50b3869f827d5ff217ac233caa18c1
Hannah <hannahumit@gmail.com>
2024-11-22 18:50:32 +0100

    everything works except add_addon_keys_from_list()

--- scripts/startup/HEAVYPOLY_HOTKEYS.py
+++ scripts/startup/HEAVYPOLY_HOTKEYS.py
@@ -16,14 +16,58 @@ import os
 import ast
 
 # Track keymap items per context
-keymap_list = {}
+keymap_registered_addon = []
+keymap_unregistered_addon = []
 
-# Utility function for keymap management (COMPLETED)
-def get_keymap_items_ctx(keymap_config: str, keymap_context: str):
-    return bpy.context.window_manager.keyconfigs[keymap_config].keymaps[keymap_context].keymap_items
+keymap_registered_global = []
+
+keymap_registered_modal = []
+keymap_unregistered_modal = []
+
+# JSON Parsing and Keymap Context Generation
+def parse_json(json_file_path):
+    
+    try:
+        with open(json_file_path, 'r') as file:
+            json_data = json.load(file)
+        if isinstance(json_data, dict):
+            return json_data
+        else:
+            print(f"Error: Expected a dictionary, but got {type(json_data).__name__}.")
+            return {}
+    except FileNotFoundError:
+        print(f"Error: The file {json_file_path} was not found.")
+        return {}
+    except json.JSONDecodeError:
+        print(f"Error: Failed to decode JSON in {json_file_path}.")
+        return {}
 
 
-# Utility function to convert json strings to sets (COMPLETED)
+# Get info from JSON and make sure there is no duplicate tuples, desired output: [('Blender', 'Animation'), ('Blender', 'Dopesheet')] 
+def json_context_to_list(json_data):
+
+    unique_contexts = set()
+    excluded_entry = ("Blender", "Transform Modal Map")
+
+    for section in json_data.values():
+        for entry in section:
+            config_context = (entry.get("keymap_config"), entry.get("keymap_context"))
+            if config_context != (None, None) and config_context != excluded_entry:
+                unique_contexts.add(config_context)
+
+    unique_contexts_list = list(unique_contexts)
+    return unique_contexts_list
+
+# A bunch of variables that are going to be used later on. (COMPLETED)
+script_dir = os.path.dirname(__file__) # directory of the file that launches the script
+json_file_path = 'keymaps.json' # file name
+json_file_path_os = os.path.join(script_dir, json_file_path) # full path
+
+json_content = parse_json(json_file_path_os) # parse JSON
+add_json_context = json_context_to_list(json_content) # turn JSON config and context into list
+
+
+# Utility function to convert json strings to sets 
 def convert_sets(data):
     """
     Recursively convert strings that look like Python sets into actual sets.
@@ -48,7 +92,12 @@ def convert_sets(data):
     return data  # Return unchanged if no conversion is needed
 
 
-# Function to add keymaps based on arguments (COMPLETED)
+# Utility function for keymap management 
+def get_keymap_items_ctx(keymap_config: str, keymap_context: str):
+    return bpy.context.window_manager.keyconfigs[keymap_config].keymaps[keymap_context].keymap_items
+
+
+# Function to add keymaps based on arguments
 def add_keymap_attrs(keymap_config: str,
                      keymap_context: str,
                      idname: str,
@@ -68,7 +117,7 @@ def add_keymap_attrs(keymap_config: str,
     keymap_items_context = get_keymap_items_ctx(keymap_config, keymap_context)
 
     # Create the keymap item
-    keymap_added = keymap_items_context.new(
+    keymap_item = keymap_items_context.new(
         idname,
         event_type,
         value_key,
@@ -85,198 +134,252 @@ def add_keymap_attrs(keymap_config: str,
 
     # Set properties for the keymap item through kwargs
     for prop_key, prop_value in kwargs.items():
-        if hasattr(keymap_added.properties, prop_key):
-            setattr(keymap_added.properties, prop_key, prop_value)
+        if hasattr(keymap_item.properties, prop_key):
+            setattr(keymap_item.properties, prop_key, prop_value)
 
-    if keymap_context not in keymap_list:
-        keymap_list[keymap_context] = []
-    keymap_list[keymap_context].append(keymap_added)
+    return [keymap_config, keymap_context, keymap_item]
 
 
-# remove keymap in default Blender setup (COMPLETED)
-def remove_keymap_attrs(keymap_config: str, keymap_context: str):
+# Function to remove keymaps based on arguments 
+def remove_keymap_attrs(keymap_config: str,
+                        keymap_context: str,
+                        idname: str,
+                        event_type: str, 
+                        value_key: str,
+                        **kwargs):
+    
     keymap_items_context = get_keymap_items_ctx(keymap_config, keymap_context)
 
-    if keymap_context not in keymap_list:
-        print(f"No keymaps to remove for context: {keymap_context}")
+    for keymap_item in keymap_items_context:
+        if keymap_item.idname == idname and keymap_item.type == event_type and value_key == keymap_item.value:
+            keymap_items_context.remove(keymap_item)
+            return [keymap_config, keymap_context, keymap_item]
+
+
+# Function to add modal keymaps based on arguments
+def add_modal_attrs(keymap_config: str,
+                    keymap_context: str,
+                    propvalue: str,
+                    event_type: str,
+                    value_key: str,
+                    **kwargs):
+    # retrieve the keymap items context
+    keymap_items_context = get_keymap_items_ctx(keymap_config, keymap_context)
+    
+    keymap_items_context.new_modal(propvalue, event_type, value_key, **kwargs)
+    return [keymap_config, keymap_context, propvalue, event_type, value_key]
+
+
+# Function to remove modal keymaps based on arguments
+def remove_modal_attrs(keymap_config: str,
+                       keymap_context: str,
+                       propvalue: str,
+                       event_type: str,
+                       value_key: str):
+    
+    # Retrieve the keymap items context
+    keymap_items_context = get_keymap_items_ctx(keymap_config, keymap_context)
+    
+    # Iterate through the keymap items to find the one matching the provided attributes
+    for keymap_item in keymap_items_context:
+        if keymap_item.propvalue == propvalue and keymap_item.type == event_type and keymap_item.value == value_key:
+            keymap_items_context.remove(keymap_item)
+            return [keymap_config, keymap_context, propvalue, event_type, value_key]
+
+###### ADDON KEYMAPS ######
+
+# add Addon Keys from JSON (use in register)
+def add_addon_keys():
+    keymap_attrs_data = convert_sets(json_content.get("add_keymap_attrs", []))
+    if not add_keymap_attrs:
+        print("Warning: No 'add_keymap_attrs' found in the JSON content.")
         return
 
-    # Remove each keymap item
-    for keymap_item in keymap_list[keymap_context]:
+    # Process addon keys
+    for keymap in keymap_attrs_data:
         try:
-            if keymap_item.as_pointer() != 0:  # Check if the item is still valid
-                keymap_items_context.remove(keymap_item)
-                print(f"Removed keymap: {keymap_item.idname} from {keymap_context}.")
-            else:
-                print("Keymap item is no longer valid.")
-        except (ReferenceError, ValueError) as e:
-            print(f"Error removing keymap item: {e}")
-
-    keymap_list[keymap_context].clear()
-
-# modify modal keymap in default Blender setup (COMPLETED)
-def modify_modal_keymap(keymap_config: str,
-                        keymap_context: str,
-                        add_modal_keymap: dict = None,
-                        remove_modal_keymap: dict = None):
-    
-    keymap_items_context = get_keymap_items_ctx(keymap_config, keymap_context)
-
-    if add_modal_keymap:
-        for item in keymap_items_context:
-            property_value = item.propvalue
-            if property_value in add_modal_keymap:
-                item.type = add_modal_keymap[property_value]
-
-    if remove_modal_keymap:
-        for item in reversed(keymap_items_context):
-            property_value = item.propvalue
-            key = item.type
-            if property_value in remove_modal_keymap and key == remove_modal_keymap[property_value]:
-                keymap_items_context.remove(item)
-    
+            # Pass the JSON keymap data to add_keymap_attrs
+            keymap_added = add_keymap_attrs(**keymap)
+            keymap_registered_addon.append(keymap_added)
+        except Exception as e:
+            print(f"Error adding Addon keymap: {e}")
 
 
-# JSON Parsing and Keymap Context Generation (COMPLETED)
-def parse_json(json_file_path):
-    
-    try:
-        with open(json_file_path, 'r') as file:
-            json_data = json.load(file)
-        if isinstance(json_data, dict):
-            return json_data
-        else:
-            print(f"Error: Expected a dictionary, but got {type(json_data).__name__}.")
-            return {}
-    except FileNotFoundError:
-        print(f"Error: The file {json_file_path} was not found.")
-        return {}
-    except json.JSONDecodeError:
-        print(f"Error: Failed to decode JSON in {json_file_path}.")
-        return {}
+# Remove Addon Keys from JSON (use in register)
+def remove_addon_keys():
+    keymap_attrs_data = convert_sets(json_content.get("remove_keymap", []))
+    if not keymap_attrs_data:
+        print("Warning: No 'remove_keymap' found in the JSON content.")
+        return
+
+    # Process addon keys
+    for keymap in keymap_attrs_data:
+        try:
+            # Pass the JSON keymap data to add_keymap_attrs
+            keymap_removed = remove_keymap_attrs(**keymap)
+            keymap_unregistered_addon.append(keymap_removed)
+        except Exception as e:
+            print(f"Error removing Addon keymap: {e}")
 
 
-# Get info from JSON and make sure there is no duplicate tuples, desired output: [('Blender', 'Animation'), ('Blender', 'Dopesheet')] (COMPLETED)
-def json_context_to_list(json_data):
-
-    unique_contexts = set()
-    excluded_entry = ("Blender", "Transform Modal Map")
-
-    for section in json_data.values():
-        for entry in section:
-            config_context = (entry.get("keymap_config"), entry.get("keymap_context"))
-            if config_context != (None, None) and excluded_entry:
-                unique_contexts.add(config_context)
-
-    unique_contexts_list = list(unique_contexts)
-    return unique_contexts_list
-
-# A bunch of variables that are going to be used later on. (COMPLETED)
-script_dir = os.path.dirname(__file__) # directory of the file that launches the script
-json_file_path = 'keymaps.json' # file name
-json_file_path_os = os.path.join(script_dir, json_file_path) # full path
-
-json_content = parse_json(json_file_path_os) # parse JSON
-add_json_context = json_context_to_list(json_content) # turn JSON config and context into list
+# remove created addon keys from list (used as a restore for default keymaps in unregister)
+def remove_addon_keys_from_list():
+    for keymap in keymap_registered_addon[:]:
+        keymap_config, keymap_context, keymap_item = keymap
+        remove_keymap_attrs(
+            keymap_config=keymap_config,
+            keymap_context=keymap_context,
+            idname=keymap_item.idname,
+            event_type=keymap_item.type,
+            value_key=keymap_item.value
+        )
+        keymap_registered_addon.remove(keymap)
 
 
-# Add and Remove global keys (COMPLETED)
-def add_remove_global_keys(add_json_context, add_remove: str):
+# restore removed addon keys from list (used as a restore for default keymaps in unregister)
+def add_addon_keys_from_list():
+    for keymap in keymap_unregistered_addon[:]:
+        keymap_config, keymap_context, keymap_item = keymap
+        add_keymap_attrs(
+            keymap_config=keymap_config,
+            keymap_context=keymap_context,
+            idname=keymap_item.idname,
+            event_type=keymap_item.type,
+            value_key=keymap_item.value
+        )
+        keymap_unregistered_addon.remove(keymap)
 
+
+###### GLOBAL KEYMAPS ######
+
+
+# add Global Keys from JSON (use in register)
+def add_global_keys(add_json_context):
     global_keys_data = json_content.get("global_keys", [])
     if not global_keys_data:
         print("Warning: No 'global_keys' found in the JSON content.")
         return
 
     # Process global keys
-    for keymap_setup in add_json_context:
+    for keymap_config, keymap_context in add_json_context:
         for keymap in global_keys_data:
-            if add_remove == "add_keymaps":
-                try:
-                    # Pass the JSON keymap data to add_keymap_attrs
-                    add_keymap_attrs(
-                        keymap_config=keymap_setup[0],
-                        keymap_context=keymap_setup[1],
-                        **keymap
-                    )
-                except Exception as e:
-                    print(f"Error adding keymap: {e}")
-
-            if add_remove == "remove_keymaps":
-                try:
-                    # Pass the JSON keymap data to add_keymap_attrs
-                    remove_keymap_attrs(
-                        keymap_config=keymap_setup[0],
-                        keymap_context=keymap_setup[1]
-                    )
-                except Exception as e:
-                    print(f"Error removing keymap: {e}")
-
-
-# Add and Remove Addon Keys (COMPLETED)
-def add_remove_addon_keys(add_remove: str):
-
-    # Ensure "global_keys" exists in the JSON content
-    keymap_attrs_data = convert_sets(json_content.get("add_keymap_attrs", []))
-    if not keymap_attrs_data:
-        print("Warning: No 'add_keymap_attrs' found in the JSON content.")
-        return
-
-    # Process addon keys
-    if add_remove == "add_keymaps":
-        for keymap in keymap_attrs_data:
             try:
                 # Pass the JSON keymap data to add_keymap_attrs
-                add_keymap_attrs(
-                    **keymap
-                )
+                keymap_added = add_keymap_attrs(keymap_config, keymap_context, **keymap)
+                keymap_registered_global.append(keymap_added)
             except Exception as e:
-                print(f"Error adding keymap: {keymap}, Error: {e}")
-
-    if add_remove == "remove_keymaps":
-        for keymap in keymap_attrs_data:
-            try:
-                # Pass the JSON keymap data to add_keymap_attrs
-                remove_keymap_attrs(
-                    keymap
-                )
-            except Exception as e:
-                print(f"Error adding keymap: {keymap}, Error: {e}")
+                print(f"Error adding Global keymap: {e}")
 
 
-def add_remove_modal_keys(add_remove: str):
+# remove created global keys from list (used as a restore for default keymaps in unregister)
+def remove_global_keys_from_list():
+    for keymap in keymap_registered_global[:]:
+        keymap_config, keymap_context, keymap_item = keymap
+        remove_keymap_attrs(
+            keymap_config=keymap_config,
+            keymap_context=keymap_context,
+            idname=keymap_item.idname,
+            event_type=keymap_item.type,
+            value_key=keymap_item.value
+        )
+        keymap_registered_global.remove(keymap)
 
-    # Ensure "modify_modal_keymap" exists in the JSON content
-    keymap_modal_data = convert_sets(json_content.get("modify_modal_keymap", []))
+
+###### MODAL KEYMAPS ######
+
+
+# add Modal Keys from JSON (use in register)
+def add_modal_keys():
+    # Ensure "add_modal_keymap" exists in the JSON content
+    keymap_modal_data = json_content.get("add_modal_keymap", [])
     if not keymap_modal_data:
-        print("Warning: No 'add_keymap_attrs' found in the JSON content.")
+        print("Warning: No 'add_modal_keymap' found in the JSON content.")
         return
     
     for keymap in keymap_modal_data:
-        if add_remove == "add_keymaps":
-            modify_modal_keymap(
-                keymap_config=keymap.get("keymap_config"),
-                keymap_context=keymap.get("keymap_context"),
-                add_modal_keymap=keymap.get("add_modal_keymap"),
-                remove_modal_keymap=keymap.get("remove_modal_keymap"),
-                )
-        if add_remove == "remove_keymaps":
-            modify_modal_keymap(
-                keymap_config=keymap.get("keymap_config"),
-                keymap_context=keymap.get("keymap_context"),
-                add_modal_keymap=keymap.get("add_modal_keymap"),
-                remove_modal_keymap=keymap.get("remove_modal_keymap"),
-                )
+        try:
+            # Pass the JSON keymap data to add_modal_attrs
+            added_map = add_modal_attrs(**keymap)
+            keymap_registered_modal.append(added_map)
+        except Exception as e:
+            print(f"Error adding Modal keymap: {keymap}, Error: {e}")
+        
+
+# Remove Modal Keys from JSON (use in register)
+def remove_modal_keys():
+    # Ensure "remove_modal_keymap" exists in the JSON content
+    keymap_modal_data = json_content.get("remove_modal_keymap", [])
+    if not keymap_modal_data:
+        print("Warning: No 'remove_modal_keymap' found in the JSON content.")
+        return
+    
+    for keymap in keymap_modal_data:
+        try:
+            # Pass the JSON keymap data to remove_modal_attrs
+            added_map = remove_modal_attrs(**keymap)
+            keymap_unregistered_modal.append(added_map)
+        except Exception as e:
+            print(f"Error removing Modal keymap: {keymap}, Error: {e}")
+
+
+# remove created modal keymaps from list (used as a restore for default keymaps in unregister)
+def remove_modal_keys_from_list():
+    for keymap in keymap_registered_modal[:]:
+        remove_modal_attrs(*keymap)
+        keymap_registered_modal.remove(keymap)
+
+# add created modal keymaps to list (used as a restore for default keymaps in unregister)
+def add_modal_keys_from_list():
+    for keymap in keymap_unregistered_modal[:]:
+        if keymap is not None:
+            add_modal_attrs(*keymap)
+            keymap_unregistered_modal.remove(keymap)
+        else:
+            print("Warning: Encountered NoneType in keymap_unregistered_modal")
+
+###### REGISTER AND UNREGISTER FUNCTIONS ######
 
 # Keymap Register/Unregister Functions
 def keymap_register():
-    add_remove_global_keys(add_json_context, "add_keymaps")
-    add_remove_addon_keys("add_keymaps")
+    keymap_registered_addon.clear()
+    keymap_unregistered_addon.clear()
 
+    keymap_registered_global.clear()
+    
+    keymap_registered_modal.clear()
+    keymap_unregistered_modal.clear()
+
+    add_addon_keys()
+    remove_addon_keys()
+
+    add_global_keys(add_json_context)
+
+    add_modal_keys()
+    remove_modal_keys()
+    print("Keymaps registered successfully.")
+
+
+# Keymap Unregister Function (UPDATED)
 def keymap_unregister():
-    add_remove_global_keys(add_json_context, "remove_keymaps")
-    add_remove_addon_keys("remove_keymaps")
-    keymap_list.clear()
+    remove_addon_keys_from_list()
+    add_addon_keys_from_list()
+
+    remove_global_keys_from_list()
+
+    remove_modal_keys_from_list()
+    add_modal_keys_from_list()
+
+    keymap_registered_addon.clear()
+    keymap_unregistered_addon.clear()
+
+    keymap_registered_global.clear()
+    
+    keymap_registered_modal.clear()
+    keymap_unregistered_modal.clear()
+    print("Keymaps unregistered successfully.")
+
+
+
 
 # Register and Unregister Functions
 def register():
